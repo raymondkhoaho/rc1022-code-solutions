e1eada19f3881ce9e489a1d24de274e47012897c
Raymond Ho <raymondkhoaho@gmail.com>
2022-10-17 18:37:24 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,53 @@
+var width = 20;
+var height = 30;
+
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 11;
+var payment = 20;
+
+var change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 80;
+var midterm = 90;
+var final = 95;
+
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Raymond ';
+var lastName = 'Ho';
+
+var fullName = firstName + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 9;
+
+var isAcidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic', typeof isAcidic);
+
+var headCount = 12;
+
+var isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+
+motto = motto + ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
