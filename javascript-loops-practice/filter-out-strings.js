9b1bcce0a860d7dcb49e5f3c02fc8cf76d1777a8
Raymond Ho <raymondkhoaho@gmail.com>
2022-10-21 16:38:28 +0000

    Revised filter-out-strings.js to filter out strings only, instead of only including numbers.

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -2,7 +2,7 @@
 function filterOutStrings(values) {
   var output = [];
   for (var i = 0; i < values.length; i++) {
-    if (typeof values[i] === 'number') {
+    if (typeof values[i] !== 'string') {
       output.push(values[i]);
     }
   }
