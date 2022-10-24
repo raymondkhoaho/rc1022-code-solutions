f048c86567f1687374e2d20ca5113cae937f32ba
Raymond Ho <raymondkhoaho@gmail.com>
2022-10-24 23:15:45 +0000

    Completed dom-manipulation exercise.

---
+++ dom-manipulation/main.js
@@ -0,0 +1,36 @@
+/* result = each click on hot button increases count (change text of click-count class in paragraph)
+each time a click count reaches a certain threshold, we want button color to change
+(change class of .hot-button.cold from cold --> cool --> tepid --> warm --> hot --> nuclear)
+
+create addeventlistener for button for a click event.
+create variable to store number of times button has been clicked.
+create new variable for DOM query for hot-button class.
+create new variable for DOM query for click-count class
+*/
+
+var clickCounter = 0;
+var $hotButton = document.querySelector('.hot-button');
+var $clickCount = document.querySelector('.click-count');
+
+function updateClickCount(event) {
+  clickCounter++;
+  $clickCount.textContent = 'Clicks: ' + clickCounter;
+
+  if (clickCounter < 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (clickCounter < 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (clickCounter < 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (clickCounter < 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (clickCounter < 16) {
+    $hotButton.className = 'hot-button hot';
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
+}
+
+$hotButton.addEventListener('click', updateClickCount);
+
+$clickCount.textContent = 'Clicks: ' + clickCounter;
