0544908237b28f8dc44d7e8f7dd97c5b02829c34
Raymond Ho <raymondkhoaho@gmail.com>
2022-10-25 18:59:04 +0000

    changed code toggle boolean value. updated seudo code.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,25 +1,21 @@
 /* result = light bulb that will turn on and off with each click
 Need a click counter that switches between statement being true or false with each click.
-  - could create if statement that compares click count to even/odd
-  -if even (divisible by two) then light bulb yellow with background yellow
-  -if odd (remainder) then light bulb yellow with black background
-declare variable to keep count of clicks and set it to null
+declare variable for click button.
 DOM query light bulb and assign to variable
 DOM query background and assign to variable
 addEventListener for click to invoke function
-function should increment click count witch each click (i++)
-if even = switch className to yellow bulb/background
-if odd = switch className to black bulb/background
+use logical NOT operator to toggle between true and false and return the result
+if true = switch className to yellow bulb/background
+if false = switch className to black bulb/background
 */
 
-var clickEvenOrOdd = null;
+var clickTrueOrFalse = null;
 var $lightBulb = document.querySelector('.light-bulb');
 var $backgroundColor = document.querySelector('.background-yellow');
 
 function clickOnAndOff(event) {
-  clickEvenOrOdd++;
-
-  if (clickEvenOrOdd % 2 === 0) {
+  clickTrueOrFalse = !clickTrueOrFalse;
+  if (clickTrueOrFalse === true) {
     $lightBulb.className = 'light-bulb yellow';
     $backgroundColor.className = 'background-yellow';
   } else {
