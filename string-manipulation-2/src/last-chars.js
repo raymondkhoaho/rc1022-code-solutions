0f7cf6af7b862e7a3d570b5d5b9a60400c67588d
Raymond Ho <raymondkhoaho@gmail.com>
2022-10-26 08:23:46 +0000

    completed first-chars and last-chars.

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,21 @@
 /* exported lastChars */
+
+/* result = return a string truncated at input length to the last index.
+input: -length representing index at which to truncate string
+       -string of any length.
+slice string at length of string counted from end of string (-length)
+create variable to represent lastindex.
+need to create if statement to account for cases where length number is more than
+length of string.
+  -only slice if number length is equal or less than the length of string
+  -else just return whole string.
+*/
+
+function lastChars(length, string) {
+  var lastIndex = string.length;
+  if (length <= string.length) {
+    return string.slice(-length, lastIndex);
+  } else {
+    return string;
+  }
+}
