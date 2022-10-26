660bc10d7651af1503a43631c35bc636f3546af6
Raymond Ho <raymondkhoaho@gmail.com>
2022-10-26 16:10:41 +0000

    in progress swap chars

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,12 @@
 /* exported swapChars */
+
+function swapChars(firstIndex, secondIndex, string) {
+  var newString = string.split('');
+  for (var i = 0; i < newString.length; i++) {
+    string.split();
+  }
+  return newString.join('');
+
+}
+
+/*  newWords[i] = newWords[i][0].toUpperCase() + newWords[i].slice(1).toLowerCase */
