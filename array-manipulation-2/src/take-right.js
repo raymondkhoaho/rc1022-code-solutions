8d16ab8b096b1aff416fbc2c03bc49cc29b93b9a
Raymond Ho <raymondkhoaho@gmail.com>
2022-10-27 21:00:30 +0000

    take-right.js in progress

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,23 @@
 /* exported takeRight */
+/* result = new array including only "count" items starting from end of array
+input: array of string and count of # of elements to take, starting at end of array
+define new function take(array, count)
+declare new variable for an empty array
+if statement to run code only if the count is less than length of array. else return an empty array.
+for statement to iterate through array until count starting from end of array (lastIndex)
+  initialization: start i at array.length
+  condition: stop at count
+  final: increment index by one
+push each array value at index that meets condition to new array.
+return new array.
+*/
+
+function takeRight(array, count) {
+  var output = [];
+  if (count <= array.length) {
+    for (var i = (array.length - count); i < array.length; i++) {
+      output.push(array[i]);
+    }
+  }
+  return output;
+}
