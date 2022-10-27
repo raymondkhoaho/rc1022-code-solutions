4a09feb20d8d17616333e9dfc02e9143b6042c73
Raymond Ho <raymondkhoaho@gmail.com>
2022-10-27 08:00:21 +0000

    Completed string-manipulation-2 challenge.

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,18 +1,20 @@
 /* exported swapChars */
 
+/* result = swap character at first and second index in any given string.
+input: string of any length
+extract each section of string:
+  string.slice(0, firstIndex) = string from index 0 cut to before firstIndex.
+  string.slice(secondIndex, secondIndex + 1) = letter of second index.
+  string.slice(firstIndex + 1, secondIndex) = words sliced from after first index to before second index.
+  string.slice(firstIndex, firstIndex + 1) = letter of first index.
+  string.slice(secondIndex + 1, string.length) = cut from after second index to end of string.
+concatenate in correct order and return new string.
+*/
+
 function swapChars(firstIndex, secondIndex, string) {
-  return string.slice(0, firstIndex) + string.slice(secondIndex, secondIndex + 1) + string.slice(firstIndex + 1, secondIndex) + string.slice(firstIndex, firstIndex + 1) + string.slice(secondIndex + 1, string.length);
-
+  return string.slice(0, firstIndex) +
+  string.slice(secondIndex, secondIndex + 1) +
+  string.slice(firstIndex + 1, secondIndex) +
+  string.slice(firstIndex, firstIndex + 1) +
+  string.slice(secondIndex + 1, string.length);
 }
-
-/*  newWords[i] = newWords[i][0].toUpperCase() + newWords[i].slice(1).toLowerCase
-var newString = string.split('');
-  for (var i = 0; i < newString.length; i++) {
-      return string.slice(0, (firstIndex + 1)) + string[secondIndex] + string.slice(firstIndex, secondIndex) + string[firstIndex] + string.slice(secondIndex, lastIndex);
-
-string.slice(firstIndex, firstIndex + 1) = letter of first index.
-string.slice(secondIndex, secondIndex + 1) = letter of second index.
-string.slice(firstIndex + 1, secondIndex) = words spliced.
-string.slice(0, firstIndex) - from index 0 cut to firstIndex.
-string.slice(secondIndex + 1, string.length) = cut from second index to end of string.
-      */
