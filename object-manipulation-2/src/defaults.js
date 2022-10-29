f92a51bee07e4e339a52ddb445613bbb99feed5d
Raymond Ho <raymondkhoaho@gmail.com>
2022-10-29 08:03:23 +0000

    Completed object-manipulation-2 challenge.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,7 +1,9 @@
 /* exported defaults */
 /* result = assign source object key/value pair to target object if not already in target.
-go through source and see if key property is in target object.
-if not in target object, then assign key/value pair of source to target object.
+go through keys in source object
+see if key property of source object is in target object. if in target property, invert statement from true to false,
+now meaning the keys of source are not in target object
+assign these key/value pair of source to target object.
 */
 
 function defaults(target, source) {
