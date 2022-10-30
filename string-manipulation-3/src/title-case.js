05292bf25163fed6e726dc7fbe8a82d14be735c5
Raymond Ho <raymondkhoaho@gmail.com>
2022-10-30 10:10:16 +0000

    in progress title-case.js

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,21 @@
 /* exported titleCase */
+/*
+define function
+split string into individual words and assign to new variable (new array of words)
+*/
+
+function titleCase(title) {
+// debugger;
+  var newTitle = '';
+  var splitTitle = title.split(' ');
+  for (var i = 0; i < splitTitle.length; i++) {
+    if (splitTitle[i].toLowerCase === 'api') {
+      newTitle += 'API' + ' ';
+    } else if (splitTitle[i].toLowerCase() === 'javascript') {
+      newTitle += 'JavaScript' + ' ';
+    } else {
+      newTitle += splitTitle[i][0].toUpperCase() + splitTitle[i].slice(1).toLowerCase() + ' ';
+    }
+  }
+  return newTitle.trim();
+}
