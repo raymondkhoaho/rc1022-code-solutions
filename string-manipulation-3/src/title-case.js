6fa4f9c0c7803b95bbee20a63c3f5cd61e24af4d
Raymond Ho <raymondkhoaho@gmail.com>
2022-10-31 15:52:53 +0000

    in progress title-case.js. need to account for one more condition ':'

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -9,7 +9,10 @@ function titleCase(title) {
   var newTitle = '';
   var splitTitle = title.split(' ');
   for (var i = 0; i < splitTitle.length; i++) {
-    if (i !== 0 && (splitTitle[i].toLowerCase() === 'in' || splitTitle[i].toLowerCase() === 'for' || splitTitle[i].toLowerCase() === 'the' || splitTitle[i].toLowerCase() === 'of' || splitTitle[i].toLowerCase() === 'and' || splitTitle[i].toLowerCase() === 'to' || splitTitle[i].toLowerCase() === 'on')) {
+    if (splitTitle[i].includes('-')) {
+      var dashedWord = splitTitle[i].split('-');
+      newTitle += dashedWord[0][0].toUpperCase() + dashedWord[0].slice(1).toLowerCase() + '-' + dashedWord[1][0].toUpperCase() + dashedWord[1].slice(1).toLowerCase() + ' ';
+    } else if (i !== 0 && (splitTitle[i].toLowerCase() === 'in' || splitTitle[i].toLowerCase() === 'for' || splitTitle[i].toLowerCase() === 'the' || splitTitle[i].toLowerCase() === 'of' || splitTitle[i].toLowerCase() === 'and' || splitTitle[i].toLowerCase() === 'to' || splitTitle[i].toLowerCase() === 'on')) {
       newTitle += splitTitle[i].toLowerCase() + ' ';
     } else if (splitTitle[i].toLowerCase() === 'api') {
       newTitle += 'API' + ' ';
