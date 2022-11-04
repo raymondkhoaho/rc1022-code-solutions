41dfb24e201eeca8d26bb96f984e70a82caca752
Raymond Ho <raymondkhoaho@gmail.com>
2022-11-04 08:08:41 +0000

    Completed zip.js

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,27 @@
 /* exported zip */
+/*
+define function zip(first, second)
+declare new variable to hold new empty array.
+compare first array length to second array length. if first <= second, run first for loop
+else run second for loop (where second array is shorter)
+in each for loop, iterate through index and pair each value of index of first array
+with value of index of second array. assign to new variable to hold pair
+push paired array to new empty array.
+return new array
+*/
+
+function zip(first, second) {
+  var newArray = [];
+  if (first.length <= second.length) {
+    for (var i = 0; i < first.length; i++) {
+      var pairedArray = [first[i], second[i]];
+      newArray.push(pairedArray);
+    }
+  } else {
+    for (var j = 0; j < second.length; j++) {
+      var pairedArray2 = [first[j], second[j]];
+      newArray.push(pairedArray2);
+    }
+  }
+  return newArray;
+}
