09667b4f8056530bf68ef37d6f065ce19ee74c25
Raymond Ho <raymondkhoaho@gmail.com>
2022-11-08 23:52:11 +0000

    Completed object-manipulation-3 challenge.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -12,7 +12,10 @@ create a deck of 52 cards, containing 13 ranks and 4 suits.
 shuffle deck using lodash shuffle method. assign to new variable.
 deal two cards with splice method and splice last two index of shuffled deck. assign to new variable.
 create for loop to iterate through the 4 players and assign each hand with two cards via previous variable.
-once hand is assigned, loop through players again and get add values of both hands.
+once hand is assigned, loop through players and players' hands and add values of both ranks. push to empty array.
+use lodash max to find max number in array.
+create one more for loop to compare max value and find matching array index. array index corresponds to players array index.
+console log players name at index.
 */
 
 var players = [
@@ -54,13 +57,31 @@ for (var k = 0; k < players.length; k++) {
   players[k].hand = dealTwoCards;
 }
 
-if (rank === 'Ace') {
-  rank = 11;
-} else if (rank === 'Queen' || rank === 'King' || rank === 'Jack') {
-  rank = 10;
-} else {
-  rank = parseInt(rank);
+function getHandValue() {
+  var handsArray = [];
+  for (var l = 0; l < players.length; l++) {
+    var handValue = 0;
+    for (var m = 0; m < players[l].hand.length; m++) {
+      if (players[l].hand[m].rank === 'Ace') {
+        handValue += 11;
+      } else if (players[l].hand[m].rank === 'Queen' || players[l].hand[m].rank === 'King' || players[l].hand[m].rank === 'Jack') {
+        handValue += 10;
+      } else {
+        handValue += parseInt(players[l].hand[m].rank);
+      }
+    }
+    handsArray.push(handValue);
+  }
+  return handsArray;
 }
-// for (var l = 0; l < players.length; l++) {
 
-// }
+var highestScore = _.max(getHandValue());
+
+function winner() {
+  for (var n = 0; n < getHandValue().length; n++) {
+    if (getHandValue()[n] === highestScore) {
+      console.log('Winning Player is:', players[n].name);
+    }
+  }
+}
+winner();
