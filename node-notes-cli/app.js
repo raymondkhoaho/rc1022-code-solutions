7e41cd578b4f1e874630af63dc5b20a093545455
Raymond Ho <raymondkhoaho@gmail.com>
2022-12-01 19:30:37 +0000

    completed user can add a note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,8 +1,21 @@
 const dataJson = require('./data.json');
+const fs = require('fs');
 
+// console.log(dataJson);
 // read
 if (process.argv[2] === 'read') {
   for (const key in dataJson.notes) {
     console.log(`${key}: ${dataJson.notes[key]}`);
   }
+}
+
+// add
+
+if (process.argv[2] === 'create') {
+  dataJson.notes[dataJson.nextId] = process.argv[3];
+  dataJson.nextId++;
+  const data = JSON.stringify(dataJson, null, 2);
+  fs.writeFile('data.json', data, err => {
+    if (err) throw err;
+  });
 }
