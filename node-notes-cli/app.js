704ec22b6a42494aca9a71d65ecf59fa4bd33059
Raymond Ho <raymondkhoaho@gmail.com>
2022-12-01 20:07:40 +0000

    combined separate if statements into if/else condition and added a else condition for invalid operations.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,31 +1,25 @@
 const dataJson = require('./data.json');
 const fs = require('fs');
-// read
+
 if (process.argv[2] === 'read') {
   for (const key in dataJson.notes) {
     console.log(`${key}: ${dataJson.notes[key]}`);
   }
-}
-// add
-if (process.argv[2] === 'create') {
+} else if (process.argv[2] === 'create') {
   dataJson.notes[dataJson.nextId] = process.argv[3];
   dataJson.nextId++;
   const data = JSON.stringify(dataJson, null, 2);
   fs.writeFile('data.json', data, err => {
     if (err) throw err;
   });
-}
-// delete
-if (process.argv[2] === 'delete') {
+} else if (process.argv[2] === 'delete') {
   const index = process.argv[3];
   delete dataJson.notes[index];
   const data = JSON.stringify(dataJson, null, 2);
   fs.writeFile('data.json', data, err => {
     if (err) throw err;
   });
-}
-// update
-if (process.argv[2] === 'update') {
+} else if (process.argv[2] === 'update') {
   const index = process.argv[3];
   const newText = process.argv[4];
   dataJson.notes[index] = newText;
@@ -33,4 +27,6 @@ if (process.argv[2] === 'update') {
   fs.writeFile('data.json', data, err => {
     if (err) throw err;
   });
+} else {
+  console.log('invalid operation');
 }
