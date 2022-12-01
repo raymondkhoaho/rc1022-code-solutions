282b51c8b0b3148bb219e9f1adffdb9c698016e6
Raymond Ho <raymondkhoaho@gmail.com>
2022-12-01 19:41:09 +0000

    Completed user can delete a note.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,19 +1,25 @@
 const dataJson = require('./data.json');
 const fs = require('fs');
-
-// console.log(dataJson);
+// console.log(dataJson.notes[5]);
 // read
 if (process.argv[2] === 'read') {
   for (const key in dataJson.notes) {
     console.log(`${key}: ${dataJson.notes[key]}`);
   }
 }
-
 // add
-
 if (process.argv[2] === 'create') {
   dataJson.notes[dataJson.nextId] = process.argv[3];
   dataJson.nextId++;
+  const data = JSON.stringify(dataJson, null, 2);
+  fs.writeFile('data.json', data, err => {
+    if (err) throw err;
+  });
+}
+// delete
+if (process.argv[2] === 'delete') {
+  const index = process.argv[3];
+  delete dataJson.notes[index];
   const data = JSON.stringify(dataJson, null, 2);
   fs.writeFile('data.json', data, err => {
     if (err) throw err;
