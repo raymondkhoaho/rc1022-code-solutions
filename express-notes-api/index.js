2f35b7b98d2a49d0f4689517f79e4c62c0858913
Raymond Ho <raymondkhoaho@gmail.com>
2022-12-06 05:46:58 +0000

    completed client can delete post by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -10,6 +10,7 @@ app.listen(3000, () => {
 
 app.use(express.json());
 
+// Client can GET list of notes
 app.get('/api/notes', (req, res) => {
   const notesArray = [];
   for (const key in dataJson.notes) {
@@ -18,6 +19,7 @@ app.get('/api/notes', (req, res) => {
   res.json(notesArray);
 });
 
+// Client can GET notes by specific id
 app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id);
   if (!Number.isInteger(id)) {
@@ -29,6 +31,7 @@ app.get('/api/notes/:id', (req, res) => {
   }
 });
 
+// Client can POST new note
 app.post('/api/notes', (req, res) => {
   const newEntry = req.body;
   if (!('content' in newEntry)) {
@@ -49,21 +52,23 @@ app.post('/api/notes', (req, res) => {
   }
 });
 
+// Client can DELETE note by id
 app.delete('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id);
   if (!Number.isInteger(id)) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (Number.isInteger(id) && !(id in dataJson.notes)) {
     res.status(404).json({ error: `cannot find note with id ${id}` });
+  } else if (id in dataJson.notes) {
+    delete dataJson.notes[id];
+    const data = JSON.stringify(dataJson, null, 2);
+    fs.writeFile('data.json', data, err => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred.' });
+        process.exit(1);
+      }
+      res.status(204).json();
+    });
   }
-  delete dataJson.notes[id];
-  const data = JSON.stringify(dataJson, null, 2);
-  fs.writeFile('data.json', data, err => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'An unexpected error occurred.' });
-      process.exit(1);
-    }
-    res.status(204).json();
-  });
 });
