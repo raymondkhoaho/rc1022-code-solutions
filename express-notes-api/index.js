975b35256dc582f62ba0101f2fa7e4b412b61dba
Raymond Ho <raymondkhoaho@gmail.com>
2022-12-06 05:39:36 +0000

    3/4 of client can delete completed. in progress.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -21,18 +21,18 @@ app.get('/api/notes', (req, res) => {
 app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id);
   if (!Number.isInteger(id)) {
-    res.status(400).send({ error: 'id must be a positive integer' });
+    res.status(400).json({ error: 'id must be a positive integer' });
   } else if (dataJson.notes[id]) {
-    res.status(200).send(dataJson.notes[id]);
+    res.status(200).json(dataJson.notes[id]);
   } else {
-    res.status(404).send({ error: 'cannot find note with id 13' });
+    res.status(404).json({ error: `cannot find note with id ${id}` });
   }
 });
 
 app.post('/api/notes', (req, res) => {
   const newEntry = req.body;
   if (!('content' in newEntry)) {
-    res.status(404).send({ error: 'content is a required field' });
+    res.status(404).json({ error: 'content is a required field' });
   } else if ('content' in newEntry) {
     newEntry.id = dataJson.nextId;
     dataJson.notes[dataJson.nextId] = newEntry;
@@ -41,10 +41,29 @@ app.post('/api/notes', (req, res) => {
     fs.writeFile('data.json', data, err => {
       if (err) {
         console.error(err);
-        res.status(500).send({ error: 'An unexpected error occurred.' });
+        res.status(500).json({ error: 'An unexpected error occurred.' });
         process.exit(1);
       }
       res.status(201).json(newEntry);
     });
   }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (!Number.isInteger(id)) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (Number.isInteger(id) && !(id in dataJson.notes)) {
+    res.status(404).json({ error: `cannot find note with id ${id}` });
+  }
+  delete dataJson.notes[id];
+  const data = JSON.stringify(dataJson, null, 2);
+  fs.writeFile('data.json', data, err => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+      process.exit(1);
+    }
+    res.status(204).json();
+  });
 });
