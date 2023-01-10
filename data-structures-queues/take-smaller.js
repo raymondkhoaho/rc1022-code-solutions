20ea59ed275808f14b8374cdd73377e859c755a1
Raymond Ho <raymondkhoaho@gmail.com>
2023-01-10 21:12:50 +0000

    completed takeSmaller

--- data-structures-queues/take-smaller.js
+++ data-structures-queues/take-smaller.js
@@ -1,5 +1,16 @@
 /* exported takeSmaller */
 
 function takeSmaller(queue) {
-
+  const first = queue.dequeue();
+  if (queue.peek() === undefined) return first;
+  const second = queue.dequeue();
+  if (second !== undefined) {
+    if (second > first) {
+      queue.enqueue(second);
+      return first;
+    } else {
+      queue.enqueue(first);
+      return second;
+    }
+  }
 }
